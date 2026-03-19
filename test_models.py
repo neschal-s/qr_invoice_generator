700fb83b80c00902b7ada8e2d47c5220564b4d28
Neschal singh <singhneschal@gmail.com>
2026-03-20 05:13:41 +0530

    removed the apis

--- test_models.py
+++ test_models.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAPrJCPFz8SdFXTSqaKqzKvp0QYqKfXnzk")
+genai.configure(api_key="API KEY")
 
 for m in genai.list_models():
     print(m.name, m.supported_generation_methods)
